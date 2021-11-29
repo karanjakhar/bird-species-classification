951b73a59363ec38c9421dc250047b35baeea0f2
Karan Jakhar <karanjakhar49@gmail.com>
2021-11-29 20:04:24 +0530

    parameter changes

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ wandb.init(project="bird_species_classification", entity="karanjakhar")
 CONFIG = {
     'device': 'cuda' if torch.cuda.is_available() else 'cpu',
     'epochs': 100000,
-    'learning_rate': 0.0001,
+    'learning_rate': 0.001,
     'momentum': 0.9,
-    'weight_decay':0,
+    'weight_decay':0.0001,
     'save_weights_epoch': 100,
     'batch_size':8,
     'best_validation_accuracy': 0,
